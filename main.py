93445d77018ad08b27eb8cd84f1c2807d5b8588a
rv87 <radioval87@yandex.ru>
2022-09-06 16:38:47 +0300

    Add CLI options

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import asyncio
 import datetime
 
 import aiofiles
+import configargparse
 
 
-async def tcp_echo_client():
+async def tcp_echo_client(host, port, history):
     reader, _ = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
+        host, port)
 
     while True:
-        async with aiofiles.open('log.txt', mode='a') as f:
+        async with aiofiles.open(history, mode='a') as f:
 
             data = await reader.read(100)
             timestamp = datetime.datetime.now().strftime("%d.%m.%y %H.%M")
@@ -24,4 +25,16 @@ async def tcp_echo_client():
                 print(message)
 
 if __name__ == '__main__':
-    asyncio.run(tcp_echo_client())
+    parser = configargparse.ArgParser()
+    parser.add_argument('--host', required=True, type=str,
+        help='Host address')
+    parser.add_argument(
+        '--port', required=True, type=str, help=('Host port')
+    )
+    parser.add_argument(
+        '--history', type=str, default='./log.txt',
+        help='Path to the log file'
+    )
+    args = parser.parse_args()
+
+    asyncio.run(tcp_echo_client(args.host, args.port, args.history))
